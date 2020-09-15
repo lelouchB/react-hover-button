88c0dc1e314141e8d8bbeaf4f53cff5559c40476
lelouchB <ashutoshs.ic18@nsut.ac.in>
2020-09-15 14:21:38 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,82 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from "react";
 
-export default function App() {
+function App() {
+  const [bg, setBg] = useState("white");
+
+  const back = {
+    background: `${bg}`,
+    height: "100vh",
+    display:"flexbox",
+
+  };
+
+  const styleB = {
+    width: "100px",
+    fontSize: "20px",
+    borderRadius: "40px",
+    border: "1px solid black",
+    color: "white",
+    margin: "0.5em 1em",
+    padding: "0.25em 1em",
+    background: "#1D499B",
+  };
+  const styleR = {
+    width: "100px",
+    fontSize: "20px",
+    borderRadius: "40px",
+    border: "1px solid black",
+    color: "#fafafa",
+    margin: "0.5em 1em",
+    padding: "0.25em 1em",
+    background: "#c83f49",
+  };
+  const styleY = {
+    width: "100px",
+    fontSize: "20px",
+    borderRadius: "40px",
+    border: "1px solid black",
+    color: "black",
+    margin: "0.5em 1em",
+    padding: "0.25em 1em",
+    background: "#F9D648",
+  };
+
+  const handleHover = (color) => {
+    setBg(color);
+  };
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div style={back}>
+      {/* <Button> White</Button> */}
+      <button
+        className="primary"
+        style={styleB}
+        onMouseEnter={() => handleHover("#1D499B")}
+        onMouseLeave={() => handleHover("white")}
+      >
+        {" "}
+        Blue
+      </button>
+
+      <button
+        className="primary"
+        style={styleR}
+        onMouseEnter={() => handleHover("#c83f49")}
+        onMouseLeave={() => handleHover("white")}
+      >
+        {" "}
+        Red
+      </button>
+      <button
+        className="primary"
+        style={styleY}
+        onMouseEnter={() => handleHover("#F9D648")}
+        onMouseLeave={() => handleHover("white")}
+      >
+        {" "}
+        Yellow
+      </button>
     </div>
   );
 }
+
+export default App;
